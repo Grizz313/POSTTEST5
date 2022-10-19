46f297bd1c8b39fe3d332ee61c390da08d407583
Achmad Fauzi <113384363+Grizz313@users.noreply.github.com>
2022-10-19 23:35:33 +0800

    Add files via upload

---
+++ POSTTEST5/phpcrud/index.php
@@ -0,0 +1,16 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('HANDCRAFTED!')?>
+
+<div class="content">
+    <h2>WELCOME TO HAND CRAFTED!</h2>
+    <p>HandCrafted was founded in 2022. Our Founder and CEO, Achmad Fauzi was inspired to start this company by his idea at such a young age.
+At HandCrafted, we encourage our community to feel free to express themself.</p>
+</div>
+
+<?=template_footer()?>
